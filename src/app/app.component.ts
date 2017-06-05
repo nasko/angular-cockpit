83a756b079cfec3aeeeeff5d5a1cb13bf241c2f1
Atanas Vasilev <atanas.vasilev@gmail.com>
2017-06-05 22:52:30 +0300

    Binding custom events across components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,4 +14,19 @@ export class AppComponent {
     }
   ];
 
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 }
